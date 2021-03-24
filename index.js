28adab63c1678f24ad7f11099a188d712286f513
Laura <lkzailac@gmail.com>
2021-03-24 14:50:55 -0400

    finished phase 2

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const csrf = require("csurf");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie:true })
 
 app.get("/", (req, res) => {
   res.render("index", { users });
 });
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render("create", {csrfToken:req.csrfToken()})
+})
 
 const users = [
   {
