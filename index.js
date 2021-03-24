ee34df30083190265a7ff97689979a756bb06d0a
hye <davidckim22@gmail.com>
2021-03-24 16:03:42 -0400

    finished phase 3

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 app.use(cookieParser());
+app.use(express.urlencoded());
 const csrfProtection = csrf({ cookie:true })
 
 app.get("/", (req, res) => {
@@ -15,6 +16,54 @@ app.get("/", (req, res) => {
 
 app.get("/create", csrfProtection, (req, res) => {
   res.render("create", {csrfToken:req.csrfToken()})
+})
+
+app.post("/create", csrfProtection, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = [];
+
+  if (!firstName) {
+    errors.push("Please provide a first name.");
+  }
+
+  if (!lastName) {
+    errors.push("Please provide a last name.");
+  }
+
+  if (!email) {
+    errors.push("Please provide an email.");
+  }
+
+  if (!password) {
+    errors.push("Please provide a password.");
+  }
+
+  if (password !== confirmedPassword) {
+    errors.push("The provided values for the password and password confirmation fields did not match.");
+  }
+
+  if (errors.length > 0) {
+    res.render("create", {
+      errors,
+      csrfToken: req.csrfToken(),
+      firstName,
+      lastName,
+      email,
+      password
+    })
+    return;
+  }
+
+  const user = {
+    id: users.length + 1,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password
+  }
+
+  users.push(user);
+  res.redirect("/");
 })
 
 const users = [
